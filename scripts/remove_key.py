529cb9945fefe6bc491b74b1ab21443bebc02ae6
chr-sa <chr-sa@noreply.codeberg.org>
2024-09-21 23:51:50 +0200

    change name of the language when removing a key

--- scripts/remove_key.py
+++ scripts/remove_key.py
@@ -4,7 +4,8 @@ from pathlib import Path
 
 file = Path("./static/language_data/ger-en20-80.json")
 # file_raw = Path("./static/language_data_raw/ger-en50-50.json")
-new_file = Path("./static/language_data/ge28.json")
+new_name = "ge28"
+new_file = Path(f"./static/language_data/{new_name}.json")
 
 with open(file, "r") as f:
     data = json.load(f)
@@ -14,6 +15,8 @@ for key in ['characters', 'bigrams', 'skipgrams', 'skipgrams2', 'skipgrams3', 't
     s = sum([v for v in data[key].values()])
     data[key] = {k: v / s for k, v in data[key].items()}
 
+data["language"] = new_name
+
 with open(new_file, "w") as f:
-    json.dump(data, f)
+    json.dump(data, f, indent=2)
 
